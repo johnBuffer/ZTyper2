37c57a08844f99b11b20d51596f96aec85f965b6
Jean Tampon <jean.tampon@gmail.com>
2020-01-26 22:37:14 +0100

    Fix color error

--- src/letter.cpp
+++ src/letter.cpp
@@ -1,12 +1,13 @@
 #include "letter.hpp"
 #include "utils.hpp"
+#include <iostream>
 
 
 Letter::Letter(const Vec2& position_, char c)
 	: PooledGameObject(position_, 0.0f)
 	, glyph(c)
 	, time(0.0f)
-	, speed(getRandRange(10.0f), -20.0f + getRandRange(5.0f))
+	, speed(getRandRange(10.0f), -7.0f + getRandRange(2.0f))
 	, angular_velocity(getRandRange(20.0f))
 {
 }
@@ -18,8 +19,8 @@ void Letter::update(float dt)
 	position += speed * time_factor;
 	angle += angular_velocity * time_factor;
 
-	speed += Vec2(0.0f, 1.5f) * time_factor;
-	//angular_velocity *= 0.95f / time_factor;
+	speed += Vec2(0.0f, 0.5f) * time_factor;
+	angular_velocity *= 0.95f / time_factor;
 
 	checkDead(this);
 }
@@ -32,8 +33,8 @@ bool Letter::isDone() const
 void Letter::render() const
 {
 	const float time_ratio = 1.0f - time / max_time;
-	const uint8_t c_intensity = static_cast<uint8_t>(255 * time_ratio);
-	sf::Color color(c_intensity);
+	const uint8_t c_intensity = static_cast<uint8_t>(255.0f * time_ratio);
+	sf::Color color(c_intensity, c_intensity, c_intensity);
 
 	auto text = create_obj<sf::Text>();
 	text->setFont(GameEngine::getInstance().resources.getFont());
@@ -44,6 +45,5 @@ void Letter::render() const
 	text->setOrigin(bounds.width * 0.5f, bounds.height * 0.5f);
 	text->setRotation(angle);
 	text->setPosition(position.x, position.y);
-
 	GameEngine::getInstance().renderer.addDrawable(text);
 }
