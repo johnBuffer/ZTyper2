fb28fa5b6c4d68a3c24f9bff40467be117372eca
Jean Tampon <jean.tampon@gmail.com>
2020-01-29 01:36:12 +0100

    Create smoke.hpp

---
+++ include/smoke.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "pooled_game_object.hpp"
+
+
+struct Smoke : PooledGameObject<Smoke>
+{
+	Smoke() = default;
+	Smoke(const Vec2& position, const Vec2& speed);
+
+	void update(float dt) override;
+	void render() const override;
+	bool isDone() const;
+	void onDone() override;
+
+	static void init();
+};
